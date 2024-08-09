f642c97e4d35173021f786ce3b03a908627991c8
Ohkrah <dion.cobb.thornhill@gmail.com>
2024-08-08 22:10:39 -0700

    Added Modal HTML code, Added some CSS and JS for Modal

--- assets/js/directory.js
+++ assets/js/directory.js
@@ -1,10 +1,10 @@
 let cards = [consoleLog, dataTypes, variables, arrays];
 //Console.log() array/variables
-const consoleLog = [CLDescription, CLExample, CLLocationBox[0],CLLocationBox[1],CLLocationBox[2],CLDraggableBox];
-const CLDescription="A built-in function that allows you to output messages or values to the console";
-const CLExample=`Console.log("Put string here");`;
-const CLLocationBox=["Hello World", `"Hello World"`, null];
-const CLDraggableBox=`Console.log("Hello World")`;
+const consoleLog = [CLDescription, CLExample, CLLocationBox[0], CLLocationBox[1], CLLocationBox[2], CLDraggableBox];
+const CLDescription = "A built-in function that allows you to output messages or values to the console";
+const CLExample = `Console.log("Put string here");`;
+const CLLocationBox = ["Hello World", `"Hello World"`, null];
+const CLDraggableBox = `Console.log("Hello World")`;
 //Data Types array/variables
 //Variables array/variables
 //Arrays array/variables
