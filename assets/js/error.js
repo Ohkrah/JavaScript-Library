4fcb782666f8476aee8e4d238b169a84d7dbeceb
Ohkrah <dion.cobb.thornhill@gmail.com>
2024-08-14 20:10:32 -0700

    added some dad joke code (not functional), added event listener and finalized search

---
+++ assets/js/error.js
@@ -0,0 +1,9 @@
+let dadJokeText = document.getElementsByClassName(`h2`)
+let joke = `$ curl -H "Accept: text/plain" https://icanhazdadjoke.com/j/R7UfaahVfFd`;
+//dadJoke function
+function dadJoke(){
+    document.getElementById(`dadJoke`).appendChild(`$ curl -H "Accept: text/plain" https://icanhazdadjoke.com/j/R7UfaahVfFd`);
+    
+}
+
+dadJoke();
